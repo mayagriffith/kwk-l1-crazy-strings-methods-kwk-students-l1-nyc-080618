728fd9794e66440c8c8e24e98fd005f57e16feb3
mayagriffith <mayaegriffith@gmail.com>
2018-08-07 14:49:27 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -3,7 +3,7 @@ def crazy_strings
   (first_string, second_string)
   first_string = first_string.upcase.reverse
   second_string = second_string.swapcase.gsub("s", "z")
-  #{first_string} #{second_string}
+  return #{first_string} #{second_string}
 end
 
  puts crazy_strings("Hello","Friends")
