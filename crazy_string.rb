7fb022951687938af825db50a70d4524283b71ed
mayagriffith <mayaegriffith@gmail.com>
2018-08-07 14:35:07 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,7 +1,9 @@
 
 def crazy_strings(a,b)
+  a = apples
+  b = oranges
 puts "okay. #{a} and #{b}".upcase.reverse
 puts "okay. #{a} and #{b}".gsub("s","z")
 end
 
-crazy_strings("apples","oranges")
+crazy_strings
