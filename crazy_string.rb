8e10a0f084219c557c9cdf3eea48a3732fa5a7f8
mayagriffith <mayaegriffith@gmail.com>
2018-08-07 14:50:15 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,6 +1,5 @@
 
-def crazy_strings
-  (first_string, second_string)
+def crazy_strings(first_string, second_string)
   first_string = first_string.upcase.reverse
   second_string = second_string.swapcase.gsub("s", "z")
   return #{first_string} #{second_string}
